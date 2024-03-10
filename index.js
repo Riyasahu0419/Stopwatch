2d291fa046265e5715a50ef3176b08093b230c08
Riya Sahu <141046650+Riyasahu0419@users.noreply.github.com>
2024-03-10 23:20:47 +0530

    this is a timing Stopwatch

--- index.js
+++ index.js
@@ -1,33 +1,55 @@
+let second=0
+let minute=0
+let hours=0
+let milisecond=0
+let time;
 
-let count=0
-let id
+let display_timer = document.querySelector(".display")
 
-// let start=document
 
-function start(){
+    function start(){
+       time= setInterval(()=>{
+            milisecond+=100;
+            if(milisecond==1000){
+                milisecond=0
+                second+=1;
+
+            }
+
+           if(second==60){
+            second=0
+            minute++;
+           } 
+           if(minute==60){
+            minute=0
+            hours++;
+           }
+           display_timer.innerText=formatTime(hours)+" : " + formatTime(minute)+ " : "+ formatTime(second)+ " : " + formatTime(milisecond);
+        },100)
+
+    }
+
     
-    id=setInterval(()=>{
-        console.log(count)
-        count+=1
-        document.getElementById('countdisplay').innerText = count;
+    function pouse(){
+        clearInterval(time)
+    }
+    function reset(){
+        second=0
+        minute=0
+        hours=0
+        milisecond=0
+
+        display_timer.innerText="00hr : 00min : 00s : 00ms"
+                
         
-    },1000)
-    
-    
-} 
+    }
+
+    // when left side timer will be 0 like hour , minute then next zero should be work in timer like sec , milisec
+
+    function formatTime(time){
+        return (time<10 ? "0" + time : time);
+    }
 
 
-function pause(){
-    
-    clearInterval(id);
-    
-}
 
-function reset(){
-    clearInterval(id);
-    count=0;
-    // document.getElementById('countdisplay').innerText = count;
 
-   
-    
-}
